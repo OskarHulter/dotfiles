8a2eede9675a922e9b193909d17d5b5a6279bc4a
Oskar Hulter <stresslessness91@gmail.com>
2023-10-14 14:59:42 +0200

    update install deps

--- install/install-deps-macos.sh
+++ install/install-deps-macos.sh
@@ -1,27 +1,30 @@
+#!/usr/bin/env bash
 # brew tap federico-terzi/espanso
 # brew install espanso
 # brew install koekeishiya/formulae/yabai
-brew install obsidian
+# brew install obsidian
 brew install fzf
 brew install zsh-syntax-highlighting
-brew install firefox
 brew install skhd
 brew install istat-menus
 brew install docker
 brew install docker-compose
 # brew install cask slack
-brew install alfred
+brew install raycast
 brew install trash
-brew install the_silver_searcher
 brew install tree
 brew install yt-dlp
 brew install jsonpp
 brew install jq
+brew install zoxide
+brew install lazygit
+brew install lazydocker
+brew install yazi
 
 # brew tap jakehilborn/jakehilborn
 # brew install displayplacer
-brew install exa
-brew install z
+brew install eza
+# brew install z
 
 # spark, fantastical, cardhop, shush, kaleidoscope
 
@@ -29,7 +32,8 @@ brew install ksdiff
 brew install whatsapp
 # brew install alt-tab
 brew install neovim
-# brew install alacritty
+brew install alacritty
+brew install vscodium
 
 brew install fnm
 brew install tmux
